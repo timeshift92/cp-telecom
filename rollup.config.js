c5a2aed78ecdc11a90f7f8b8cccfb14b4ea44fb3
timeshift <akhmedov@infosystems.uz>
2021-06-12 18:48:36 +0500

    refactor

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,7 @@ import { injectManifest } from 'rollup-plugin-workbox'
 
 const { distDir } = getConfig() // use Routify's distDir for SSOT
 const assetsDir = 'assets'
-const buildDir = `${distDir}/build`
+const buildDir = `${distDir}`
 const isNollup = !!process.env.NOLLUP
 const production = !process.env.ROLLUP_WATCH;
 
