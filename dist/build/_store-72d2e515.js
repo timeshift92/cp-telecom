ac63aa9b4dcbd83eeacfa37886a56bed877fb42b
timeshift <akhmedov@infosystems.uz>
2021-06-19 09:43:41 +0500

    changes

--- dist/build/_store-72d2e515.js
+++
@@ -1,2 +0,0 @@
-import{L as o}from"./main.js";const s=o(!1);export{s as u};
-//# sourceMappingURL=_store-72d2e515.js.map
--- dist/build/feature2-33355f8d.js
+++
@@ -1,2 +0,0 @@
-import{S as t,i as e,s,e as a,b as n,d as r,n as o,f as u}from"./main.js";function i(t){let e,s,i;return{c(){e=a("h1"),e.textContent="Feature 2",s=n(),i=a("b"),i.textContent="v1.1 feature"},m(t,a){r(t,e,a),r(t,s,a),r(t,i,a)},p:o,i:o,o:o,d(t){t&&u(e),t&&u(s),t&&u(i)}}}export default class extends t{constructor(t){super(),e(this,t,null,i,s,{})}}
-//# sourceMappingURL=feature2-33355f8d.js.map
